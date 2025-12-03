370261214c2941da1eed6ee699f8f4770c1a270f
Alan Benny <alanbenny@gmail.com>
2025-12-03 09:48:32 -0500

    latest

--- music_connect/connectors/youtube_connector/app/oauth_handler.py
+++ music_connect/connectors/youtube_connector/app/oauth_handler.py
@@ -9,7 +9,7 @@ from googleapiclient.discovery import build
 
 
 CLIENT_SECRET_FILE = os.getenv("YOUTUBE_CLIENT_SECRET_FILE")
-SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/youtube"]
 
 def get_flow():
     return Flow.from_client_secrets_file(
