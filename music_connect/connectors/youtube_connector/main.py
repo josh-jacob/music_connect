1d290f22bc934410c3f92163e6c8b4086db258a5
josh-jacob <144190443+josh-jacob@users.noreply.github.com>
2025-12-03 02:59:29 -0500

    Resolve merge conflicts in YouTube connector

--- music_connect/connectors/youtube_connector/main.py
+++ music_connect/connectors/youtube_connector/main.py
@@ -4,16 +4,12 @@ load_dotenv()
 from fastapi.responses import RedirectResponse
 from oauth_handler import get_flow, get_authenticated_service
 from token_storage import save_tokens
-<<<<<<< HEAD
 from pydantic import BaseModel
-=======
->>>>>>> main
 
 
 import os
 
 app = FastAPI()
-<<<<<<< HEAD
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -28,8 +24,6 @@ app.add_middleware(
     allow_headers=["*"],          # Allow all headers
 )
 
-=======
->>>>>>> main
 """
 Redirect user to Google's OAuth page.
 Requests offline access so we get a refresh token.
@@ -62,10 +56,6 @@ def youtube_callback(request: Request):
         "expires_at": credentials.expiry.isoformat(),
         "scopes": credentials.scopes
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> main
     save_tokens(token_data)
 
     return {"message": "YouTube connected and tokens saved!"}
@@ -98,7 +88,6 @@ def get_playlist_items(playlist_id: str):
         ).execute()
 
         for item in response["items"]:
-<<<<<<< HEAD
             snippet = item["snippet"]
 
             items.append({
@@ -110,16 +99,6 @@ def get_playlist_items(playlist_id: str):
             })
 
 
-=======
-            items.append({
-                "title": snippet["title"],
-                "videoId": item["contentDetails"]["videoId"],
-                "thumbnail": snippet["thumbnails"]["default"]["url"],
-                "channel": snippet.get("videoOwnerChannelTitle")  # preferred field
-                            or snippet.get("channelTitle")        # fallback for older videos
-            })
-
->>>>>>> main
         next_page_token = response.get("nextPageToken")
         if not next_page_token:
             break
@@ -264,7 +243,6 @@ def remove_track_from_playlist(playlist_id: str, videoId: str):
         "videoId": videoId
     }
 
-<<<<<<< HEAD
     from pydantic import BaseModel
 
 class CreatePlaylistRequest(BaseModel):
@@ -337,5 +315,3 @@ def get_user_info():
     }
 
 
-=======
->>>>>>> main
