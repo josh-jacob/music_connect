84441e5d7f98821c055e812c7627d2b2bc000d8b
OmarAlkwifi <159771450+OmarAlkwifi@users.noreply.github.com>
2025-12-04 15:06:35 -0500

    Update ServiceButton.js

--- music_connect_ui/src/components/ServiceButton.js
+++ music_connect_ui/src/components/ServiceButton.js
@@ -2,10 +2,18 @@ import "./ServiceButton.css";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Card} from "@mui/material";
 
-const ServiceButton = ({service, serviceURL, icon, colour}) => {
+const ServiceButton = ({service, serviceURL, icon, colour, onClick}) => {
+    const handleClick = (e) => {
+        if (onClick) {
+            e.preventDefault();
+            onClick();
+        }
+        
+    };
+
     return (
         <Card className="ServiceButton" component="section">
-            <a className={service} href={serviceURL}>
+            <a className={service} href={serviceURL} onClick={handleClick}>
                 <FontAwesomeIcon icon={icon} size="5x" color={colour}/>
                 <h3 style={{ color: "black" }}>{service}</h3>
             </a>
